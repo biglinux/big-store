040740c8b76f796a3d6ba18f1c9bf68dea2fe803
eltonff <eltonfabricio10@gmail.com>
2021-09-09 20:24:02 -0300

    ok

--- big-store/usr/share/bigbashview/bcc/apps/big-store/load.sh
+++ big-store/usr/share/bigbashview/bcc/apps/big-store/load.sh
@@ -4,12 +4,12 @@ case $1 in
     pkg_not_installed)
 		pacman -Flq "$2" | sed 's|^|/|'
     ;;
-    
+
     pkg_installed)
         pacman -Qk "$2"
 		pacman -Qlq "$2"
 	;;
-	
+
     pkg_installed_flatpak)
     echo "Folder base: $(flatpak info --show-location "$2")"
     find $(flatpak info --show-location "$2") | sed "s|$(flatpak info --show-location "$2")||g"
