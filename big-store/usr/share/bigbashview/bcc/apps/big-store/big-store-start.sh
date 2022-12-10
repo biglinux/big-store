bf46c1fa0fb3a53635d006ac108779edc8033e48
Bruno Gonçalves <bigbruno@gmail.com>
2022-12-10 12:50:03 -0300

    Add support to open in app program, like: [200~# Open Store searching for gimp # big-store gimp
    # Show Native page of gimp
    # big-store appstream gimp
    
    # Show AUR page of jre8
    # big-store aur jre8
    
    # Show Snap page of Gimp
    # big-store snap KDHYbyuzZukmLhiogKiUksByRhXD2gYV
    
    # Show Flatpak page of Gimp
    # big-store flatpak org.gimp.GIMP~

--- big-store/usr/share/bigbashview/bcc/apps/big-store/big-store-start.sh
+++ big-store/usr/share/bigbashview/bcc/apps/big-store/big-store-start.sh
@@ -49,9 +49,32 @@ echo "$(xrandr | grep primary | sed 's|.*primary ||g;s|+.*||g;s|.*x||g') / 2" | 
 if [ "$1" != "" ]; then
     if [ "$1" = "category" ]; then
       QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 WINIT_UNIX_BACKEND=x11 GDK_BACKEND=x11 bigbashview  -n "$TITLE" -w maximized index.sh.htm?category="$2" -i icon.svg
+    elif [ "$1" = "appstream" ]; then
+      QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 WINIT_UNIX_BACKEND=x11 GDK_BACKEND=x11 bigbashview  -n "$TITLE" -w maximized view_appstream.sh.htm?pkg_name="$2" -i icon.svg
+    elif [ "$1" = "aur" ]; then
+      QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 WINIT_UNIX_BACKEND=x11 GDK_BACKEND=x11 bigbashview  -n "$TITLE" -w maximized view_aur.sh.htm?pkg_name="$2" -i icon.svg
+    elif [ "$1" = "flatpak" ]; then
+      QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 WINIT_UNIX_BACKEND=x11 GDK_BACKEND=x11 bigbashview  -n "$TITLE" -w maximized view_flatpak.sh.htm?pkg_name="$2" -i icon.svg
+    elif [ "$1" = "snap" ]; then
+      QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 WINIT_UNIX_BACKEND=x11 GDK_BACKEND=x11 bigbashview  -n "$TITLE" -w maximized view_snap.sh.htm?pkg_id="$2" -i icon.svg
     else
       QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 WINIT_UNIX_BACKEND=x11 GDK_BACKEND=x11 bigbashview  -n "$TITLE" -w maximized index.sh.htm?search="$1" -i icon.svg
     fi
 else
     QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 WINIT_UNIX_BACKEND=x11 GDK_BACKEND=x11 bigbashview  -n "$TITLE" -w maximized index.sh.htm -i icon.svg
 fi
+
+# Open Store searching for gimp
+# big-store gimp
+
+# Show Native page of gimp
+# big-store appstream gimp
+
+# Show AUR page of jre8
+# big-store aur jre8
+
+# Show Snap page of Gimp
+# big-store snap KDHYbyuzZukmLhiogKiUksByRhXD2gYV
+
+# Show Flatpak page of Gimp
+# big-store flatpak org.gimp.GIMP
