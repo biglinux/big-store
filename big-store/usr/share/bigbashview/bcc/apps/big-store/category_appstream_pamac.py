776857451e85e9d54a13bad4e4aeadcaa4937486
vcatafesta <vcatafesta@gmail.com>
2023-08-21 18:04:06 -0400

    seg 21 ago 2023 18:04:05 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- big-store/usr/share/bigbashview/bcc/apps/big-store/category_appstream_pamac.py
+++ big-store/usr/share/bigbashview/bcc/apps/big-store/category_appstream_pamac.py
@@ -33,128 +33,112 @@ lang_translations.install()
 _ = lang_translations.gettext
 TMP_FOLDER = os.environ["TMP_FOLDER"]
 
-
 def print_pkg_details(details):
-    try:
-        print(
-            '<a onclick="disableBody();" href="view_appstream.sh.htm?pkg_name='
-            + details.get_name()
-            + '">'
+    print(
+        '<a onclick="disableBody();" href="view_appstream.sh.htm?pkg_name='
+        + details.get_name()
+        + '">'
+    )
+    print('<div class="col s12 m6 l3"')
+    if details.get_repo() is None:
+        file_path = TMP_FOLDER + "/category_aur.txt"
+        with open(file_path, "a") as f:
+            print(details.get_name(), file=f)
+
+    if details.get_installed_version() is not None:
+        print('id="AppstreamP1">')
+    else:
+        print('id="AppstreamP2">')
+
+    print('<div class="showapp">')
+
+    if details.get_icon() is None:
+        find_icon = subprocess.run(
+            [
+                "find",
+                "icons/",
+                "/var/lib/flatpak/appstream/flathub/x86_64/active/icons/64x64/",
+                "/usr/share/app-info/icons/archlinux-arch-community/64x64/",
+                "/usr/share/app-info/icons/archlinux-arch-extra/64x64/",
+                "-type",
+                "f",
+                "-iname",
+                "*" + details.get_name().split("-")[0] + "*",
+                "-print",
+                "-quit",
+            ],
+            stdout=subprocess.PIPE,
+            text=True,
         )
-        print('<div class="col s12 m6 l3"')
-        if details.get_repo() is None:
-            file_path = TMP_FOLDER + "/category_aur.txt"
-            try:
-                with open(file_path, "a") as f:
-                    print(details.get_name(), file=f)
-            except Exception as e:
-                print("Ocorreu uma exceção:", e)
-
-        if details.get_installed_version() is not None:
-            print('id="AppstreamP1">')
-        else:
-            print('id="AppstreamP2">')
-
-        print('<div class="showapp">')
-
-        if details.get_icon() is None:
-            find_icon = subprocess.run(
-                [
-                    "find",
-                    "icons/",
-                    "/var/lib/flatpak/appstream/flathub/x86_64/active/icons/64x64/",
-                    "/usr/share/app-info/icons/archlinux-arch-community/64x64/",
-                    "/usr/share/app-info/icons/archlinux-arch-extra/64x64/",
-                    "-type",
-                    "f",
-                    "-iname",
-                    "*" + details.get_name().split("-")[0] + "*",
-                    "-print",
-                    "-quit",
-                ],
-                stdout=subprocess.PIPE,
-                text=True,
+        if find_icon.stdout == "":
+            print(
+                "<div id=appstream_icon><div class=icon_middle><div class=avatar_appstream>"
+                + details.get_name()[0:3]
+                + "</div></div>"
             )
-            if find_icon.stdout == "":
-                print(
-                    "<div id=appstream_icon><div class=icon_middle><div class=avatar_appstream>"
-                    + details.get_name()[0:3]
-                    + "</div></div>"
-                )
-            else:
-                print(
-                    '<div id=appstream_icon><div class=icon_middle><img class="icon" loading="lazy" src="',
-                    find_icon.stdout,
-                    '"></div>',
-                )
         else:
             print(
                 '<div id=appstream_icon><div class=icon_middle><img class="icon" loading="lazy" src="',
-                details.get_icon(),
+                find_icon.stdout,
                 '"></div>',
             )
+    else:
         print(
-            "<div id=appstream_name><div id=limit_title_name>",
-            details.get_id(),
-            "</div>",
+            '<div id=appstream_icon><div class=icon_middle><img class="icon" loading="lazy" src="',
+            details.get_icon(),
+            '"></div>',
         )
-        print("<div id=version>", details.get_version(), "</div></div></div>")
+    print(
+        "<div id=appstream_name><div id=limit_title_name>",
+        details.get_id(),
+        "</div>",
+    )
+    print("<div id=version>", details.get_version(), "</div></div></div>")
 
-        if os.path.exists(
-            "description/"
-            + details.get_name()
-            + "/"
-            + locale.getlocale()[0]
-            + "/summary"
-        ):
-            print("<div id=box_appstream_desc><div id=appstream_desc>")
-            print(
-                open(
-                    "description/"
-                    + details.get_name()
-                    + "/"
-                    + locale.getlocale()[0]
-                    + "/summary",
-                    "r",
-                ).read()
-            )
-            print("</div></div>")
-        else:
-            print(
-                "<div id=box_appstream_desc><div id=appstream_desc>",
-                details.get_desc(),
-                "</div></div>",
-            )
+    if os.path.exists(
+        "description/" + details.get_name() + "/" + locale.getlocale()[0] + "/summary"
+    ):
+        print("<div id=box_appstream_desc><div id=appstream_desc>")
+        print(
+            open(
+                "description/"
+                + details.get_name()
+                + "/"
+                + locale.getlocale()[0]
+                + "/summary",
+                "r",
+            ).read()
+        )
+        print("</div></div>")
+    else:
+        print(
+            "<div id=box_appstream_desc><div id=appstream_desc>",
+            details.get_desc(),
+            "</div></div>",
+        )
 
-        if details.get_installed_version() is None:
-            print(
-                "<div id=appstream_not_installed>"
-                + _("Instalar")
-                + "</div></a></div></div>"
-            )
-        else:
-            try:
-                file_path = TMP_FOLDER + "/upgradeable.txt"
-                if os.path.exists(file_path):
-                    with open(file_path, "r+") as f:
-                        content = f.read()
-                        if "\n" + details.get_name() + "\n" in content:
-                            print(
-                                "<div id=appstream_upgradable>"
-                                + _("Atualizar")
-                                + "</div></a></div></div>"
-                            )
-                        else:
-                            print(
-                                "<div id=appstream_installed>"
-                                + _("Remover")
-                                + "</div></a></div></div>"
-                            )
-            except Exception as e:
-                print("Ocorreu uma exceção:", e)
-
-    except Exception as e:
-        print("Ocorreu uma exceção:", e)
+    if details.get_installed_version() is None:
+        print(
+            "<div id=appstream_not_installed>"
+            + _("Instalar")
+            + "</div></a></div></div>"
+        )
+    else:
+        file_path = TMP_FOLDER + "/upgradeable.txt"
+        with open(file_path) as f:
+            content = f.read()
+            if "\n" + details.get_name() + "\n" in content:
+                print(
+                    "<div id=appstream_upgradable>"
+                    + _("Atualizar")
+                    + "</div></a></div></div>"
+                )
+            else:
+                print(
+                    "<div id=appstream_installed>"
+                    + _("Remover")
+                    + "</div></a></div></div>"
+                )
 
 
 if __name__ == "__main__":
